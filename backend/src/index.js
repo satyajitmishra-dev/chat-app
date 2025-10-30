0571ea34b938d4adeb9e612c4dff7ab4a434fcc4
satyajitmishra-dev <satyajitmishra791@gmail.com>
2025-10-30 13:17:55 +0530

    Add project structure and integrate backend and frontend dependencies

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,9 +6,11 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import messageRoutes from '../src/routes/message.route.js'
 import { app, server } from './lib/socket.js';
-
+import path from 'path';
 dotenv.config();
 
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 
 // ✅ 1. Setup middlewares FIRST
@@ -27,11 +29,18 @@ app.use(cors({
 app.use("/api/auth", auth);
 app.use("/api/message", messageRoutes)
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+  app.get('*', (req, res) => {  
+    res.sendFile(path.join(__dirname, '/frontend', 'dist', 'index.html'));
+  });
+}
+
 //  3. Connect to DB
 connectDB();
 
 //  4. Start server
-const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`✅ Server is running on port ${PORT}`);
 });
